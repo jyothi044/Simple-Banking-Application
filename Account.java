4298845add80e1665def6ddf9603791b3d956089
BOMMINAYANI JYOTHI <126855483+jyothi044@users.noreply.github.com>
2024-08-06 12:09:18 +0530

    First Commit

---
+++ Account.java
@@ -0,0 +1,125 @@
+import java.util.Scanner;
+
+public class Account{
+    int balance;
+    int prevTransaction;
+    String userName;
+    String userID;
+
+    Account(String uname, String uid){
+        this.userName = uname;
+        this.userID = uid; 
+    }
+
+    //deposit money
+    public void deposit(int amount){
+        if(amount != 0){
+            balance = balance + amount;
+            prevTransaction = amount;
+        }
+    }
+
+    //withdrawl money
+
+    public void withdraw(int amount){
+        if(amount != 0 ){
+            balance = balance - amount;
+            prevTransaction = amount;
+        }
+    }
+
+    //showing the previous Transaction
+
+      public void  getPreviousTransaction(){
+        if(prevTransaction > 0){
+            System.out.println("Deposited: " + prevTransaction);
+        }
+        else if(prevTransaction < 0){
+            System.out.println("Withdrawn:" +Math.abs(prevTransaction));
+        }
+        else{
+            System.out.println("No transaction occurred");
+        }
+
+    }
+
+    //calculating interest of current funds after a number of years
+    public void Interest(int years){
+        double interestRate = .0185;
+        double newBalance = (balance * interestRate * years) + balance;
+        System.out.println("The current interest rate is" + (100 * interestRate) + "%");
+        System.out.println("After " + years + " years, you balance will be: " + newBalance);
+    }
+
+    //showing the main menu
+    void showMenu() {
+		char option = '\0';
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome, " + userName + "!");
+		System.out.println("Your ID is: " + userID);
+		System.out.println();
+		System.out.println("What would you like to do?");
+		System.out.println();
+		System.out.println("A. Check your balance");
+		System.out.println("B. Make a deposit");
+		System.out.println("C. Make a withdrawal");
+		System.out.println("D. View previous transaction");
+		System.out.println("E. Calculate interest");
+		System.out.println("F. Exit");
+		
+		do {
+			System.out.println();
+			System.out.println("Enter an option: ");
+			char option1 = scanner.next().charAt(0);
+			option = Character.toUpperCase(option1);
+			System.out.println();
+			
+			switch(option) {
+			//Case 'A' allows the user to check their account balance
+			case 'A':
+				System.out.println("====================================");
+				System.out.println("Balance = $" + balance);
+				System.out.println("====================================");
+				System.out.println();
+				break;
+			//Case 'B' allows the user to deposit money into their account using the 'deposit' function
+			case 'B':
+				System.out.println("Enter an amount to deposit: ");
+				int amount = scanner.nextInt();
+				deposit(amount);
+				System.out.println();
+				break;
+			//Case 'C' allows the user to withdraw money from their account using the 'withdraw' function
+			case 'C':
+				System.out.println("Enter an amount to withdraw: ");
+				int amount2 = scanner.nextInt();
+				withdraw(amount2);
+				System.out.println();
+				break;
+			//Case 'D' allows the user to view their most recent transaction using the 'getPreviousTransaction' function
+			case 'D':
+				System.out.println("====================================");
+				getPreviousTransaction();
+				System.out.println("====================================");
+				System.out.println();
+				break;
+			//Case 'E' calculates the accrued interest on the account after a number of years specified by the user
+			case 'E':
+				System.out.println("Enter how many years of accrued interest: ");
+				int years = scanner.nextInt();
+				Interest(years);
+				break;
+			//Case 'F' exits the user from their account
+			case 'F':
+				System.out.println("====================================");
+				break;
+			//The default case let's the user know that they entered an invalid character and how to enter a valid one
+			default:
+				System.out.println("Error: invalid option. Please enter A, B, C, D, or E or access services.");
+				break;
+			}
+		} while(option != 'F');
+		System.out.println("Thank you for banking with us!");
+    }
+}
+
---
+++ FirstBank.java
@@ -0,0 +1,12 @@
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account ac = new Account("jyo", "195");
+		ac.showMenu();
+		
+		
+	}
+
+} 
